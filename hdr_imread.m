7d161ecbf345d5b131755951c6c3f345ae9ebeaf
Jean-Francois Lalonde <jf.lalonde@disneyresearch.com>
2013-03-18 11:54:50 -0400

    Simplified and documented the dcraw options when reading a RAW image file.

--- hdr_imread.m
+++ hdr_imread.m
@@ -1,10 +1,16 @@
 function im = hdr_imread(filename, varargin)
-% Wrapper for imread that supports HDR formats
+% Wrapper for imread that supports HDR formats.
 % 
 %   im = hdr_imread(filename, ...)
 %
+% Note:
+%   Uses the command-line executable 'dcraw' to read RAW files. See:
+%   http://www.cybercom.net/~dcoffin/dcraw/
+%
 % See also:
 %   imread
+%   exrread
+%   dcraw 
 %
 % ----------
 % Jean-Francois Lalonde
@@ -22,7 +28,15 @@ switch lower(ext)
         
     case {'.nef', '.cr2'}
         % Nikon/Canon RAW formats, use dcraw to convert.
-        cmd = sprintf('dcraw -v -6 -T -W -g 1 1 -w %s', filename);
+        % options used are:
+        %   -v     => verbose
+        %   -4     => linear 16-bits
+        %   -T     => save in .tiff format
+        %          => default white balance behavior (D65 light source)
+        %   -o 0   => no color profile
+        %   -q 3   => highest possible Bayer interpolation
+        % See http://www.guillermoluijk.com/tutorial/dcraw/index_en.htm
+        cmd = sprintf('dcraw -v -4 -T -q 3 -o 0 %s', filename);
         system(cmd);
         
         % Read the generated tiff file
