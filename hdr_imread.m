10ef333a34a983e5f6fa3edbc57653cb9d4a41b8
Jean-Francois Lalonde <jf.lalonde@disneyresearch.com>
2013-03-28 14:47:05 -0400

    Automatically rotating JPG images in hdr_imread.
    Uses the EXIF information to do so.

--- hdr_imread.m
+++ hdr_imread.m
@@ -1,6 +1,6 @@
 function im = hdr_imread(filename, varargin)
 % Wrapper for imread that supports HDR formats.
-% 
+%
 %   im = hdr_imread(filename, ...)
 %
 % See also:
@@ -22,18 +22,56 @@ switch lower(ext)
         % openEXR format
         im = exrread(filename);
         
-    case {'.nef', '.cr2'}       
+    case {'.nef', '.cr2'}
         % First, convert to tiff
         tiffFile = raw2tiff(filename);
         
         % Read the generated tiff file
         im = im2double(imread(tiffFile));
         
-        % Clean up 
+        % Clean up
         delete(tiffFile);
         
     otherwise
         % other image formats supported by imread
-        im = im2double(imread(filename, varargin{:}));
+        im = im2double(imreadAutoRot(filename, varargin{:}));
         
+end
+
+    function im = imreadAutoRot(filename)
+        % Handle auto-rotation in JPEG (from EXIF tag).
+        %
+        % ----------
+        % Jianxiong Xiao
+        % Reference: http://www.impulseadventure.com/photo/exif-orientation.html
+        
+        im = imread(filename);
+        
+        info = imfinfo(filename);
+        if ~isempty(info)
+            switch info.Orientation
+                case 1
+                    
+                case 2
+                    im = im(:,end:-1:1,:);
+                case 3
+                    im = imrotate(im,180);
+                    im = im(:,end:-1:1,:);
+                case 4
+                    im = im(:,end:-1:1,:);
+                    
+                case 6
+                    im = imrotate(im,-90);
+                case 5
+                    im = im(:,end:-1:1,:);
+                    im = imrotate(im,-90);
+                    
+                case 8
+                    im = imrotate(im,90);
+                case 7
+                    im = imrotate(im,90);
+                    im = im(:,end:-1:1,:);
+            end
+        end
+    end
 end
