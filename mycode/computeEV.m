714458ddcd5c04416ec40da4e523e6e4ffb24bda
Jean-Francois Lalonde <jf.lalonde@disneyresearch.com>
2013-04-01 15:56:05 -0400

    Computing EV in base ISO100 (i.e. now subtracting log2(ISO/100)!)
    Updated comments accordingly. Thanks to Miguel for catching this!

--- mycode/computeEV.m
+++ mycode/computeEV.m
@@ -1,12 +1,15 @@
-function ev = computeEV(N, t, iso)
-% Computes the Exposure Value (EV)
+function ev100 = computeEV(N, t, iso)
+% Computes the Exposure Value (EV) at ISO 100.
 %
 %   ev = computeEV(N, t, <iso>)
 %
-% Formally, ev = log2(N^2/t)+log2(ISO/100) where 
+% Formally, ev100 = log2(N^2/t)-log2(ISO/100) where 
 %   - N is the exposure (f-stops)
 %   - t is the shutter time (in seconds)
 %   - iso is the ISO speed (default = 100)
+%
+% Taken from:
+%   http://en.wikipedia.org/wiki/Exposure_value
 %
 % ----------
 % Jean-Francois Lalonde
@@ -16,4 +19,4 @@ if nargin == 2
     iso = 100;
 end
 
-ev = log2(N^2/t)+log2(iso/100);
+ev100 = log2(N^2/t)-log2(iso/100);
