c146685a63a33c0460589827b2827ff3dbf032b1
Jean-Francois Lalonde <jflalonde@gmail.com>
2013-11-27 10:01:51 -0500

    Added Makefile for MatlabEXR
    for convenience

---
+++ 3rd_party/MatlabEXR/make.m
@@ -0,0 +1,3 @@
+mex exrread.cpp  -lIlmImf -lIex -lImath -lHalf -I/usr/local/include/OpenEXR -L/usr/local/lib
+mex exrwrite.cpp -lIlmImf -lIex -lImath -lHalf -I/usr/local/include/OpenEXR -L/usr/local/lib
+mex exrinfo.cpp  -lIlmImf -lIex -lImath -lHalf -I/usr/local/include/OpenEXR -L/usr/local/lib
