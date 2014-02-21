fa4256c79e966554dbb9ea4ad401c5e4da3f3d4b
Jean-Francois Lalonde <jflalonde@gmail.com>
2014-02-20 21:17:44 -0500

    Added option to autorotate the image or not in hdr_imread

--- mycode/hdr_imread.m
+++ mycode/hdr_imread.m
@@ -3,6 +3,12 @@ function im = hdr_imread(filename, varargin)
 %
 %   im = hdr_imread(filename, ...)
 %
+% Additional options can be used as optional inputs to imread. In addition,
+% it also supports:
+%   - 'autoRotate' [true]: rotate according to the EXIF information
+%   - 'fullRaw' [false]: 
+%   
+%
 % See also:
 %   imread
 %   exrread
@@ -19,6 +25,15 @@ fullRawVal = false;
 if ~isempty(fullRawInd)
     fullRawVal = varargin{fullRawInd+1};
 end
+varargin(fullRawInd:fullRawInd+1) = [];
+
+% check if we've passed the 'autoRotate' option. Strip if out if yes.
+autoRotateInd = find(strcmp(varargin, 'autoRotate'));
+autoRotate = true;
+if ~isempty(autoRotateInd)
+    autoRotate = varargin{autoRotateInd+1};
+end
+varargin(autoRotateInd:autoRotateInd+1) = [];
 
 if fullRawVal
     switch lower(ext)
@@ -50,6 +65,8 @@ switch lower(ext)
             im = im(:,:,1:3);
         end
         
+        im = rot(im, filename);
+        
         % Clean up
         delete(tiffFile);
         
@@ -59,42 +76,48 @@ switch lower(ext)
         
 end
 
-    function im = imreadAutoRot(filename)
+    function im = imreadAutoRot(filename, varargin)
         % Handle auto-rotation in JPEG (from EXIF tag).
         %
         % ----------
         % Jianxiong Xiao
         % Reference: http://www.impulseadventure.com/photo/exif-orientation.html
         
-        im = imread(filename);
-        
+        im = imread(filename, varargin{:});
+        im = rot(im, filename);
+    end
+
+    function im = rot(im, filename)
+        if ~autoRotate
+            return;
+        end
         info = imfinfo(filename);
-        if ~isempty(info)
-            if isfield(info, 'Orientation')
-                switch info.Orientation
-                    case 1
-                        
-                    case 2
-                        im = im(:,end:-1:1,:);
-                    case 3
-                        im = imrotate(im,180);
-                        im = im(:,end:-1:1,:);
-                    case 4
-                        im = im(:,end:-1:1,:);
-                        
-                    case 6
-                        im = imrotate(im,-90);
-                    case 5
-                        im = im(:,end:-1:1,:);
-                        im = imrotate(im,-90);
-                        
-                    case 8
-                        im = imrotate(im,90);
-                    case 7
-                        im = imrotate(im,90);
-                        im = im(:,end:-1:1,:);
-                end
+        if ~isempty(info) && isfield(info, 'Orientation')
+            switch info(1).Orientation
+                case 1
+                    
+                case 2
+                    im = im(:,end:-1:1,:);
+                case 3
+                    im = imrotate(im,180);
+                    % think that's a bug! should just be rotated
+%                     im = im(:,end:-1:1,:);
+                case 4
+                    im = im(:,end:-1:1,:);
+                    
+                case 6
+                    im = imrotate(im,-90);
+                case 5
+                    im = im(:,end:-1:1,:);
+                    im = imrotate(im,-90);
+                    
+                case 8
+                    im = imrotate(im,90);
+                case 7
+                    im = imrotate(im,90);
+                    im = im(:,end:-1:1,:);
             end
+
         end
     end
 end
