4bc00db1a62ad3a2357cc638e31f982db26d02d9
Jean-Francois Lalonde <jflalonde@gmail.com>
2014-09-30 10:42:16 -0400

    Removed old code.

--- 3rd_party/public_opthdr/m_load_et.m
+++
@@ -1,9 +0,0 @@
-function t = m_load_et(basedir, vlab)
-% read exposure times
-etfn = strcat(basedir, '/', vlab, '/exposure_times');
-disp(sprintf('  loading %s...', etfn));
-etf = fopen(etfn, 'r');
-t = fscanf(etf, '%f');
-fclose(etf);
-disp(sprintf('  inverting exposures times'));
-t = 1./t;
--- 3rd_party/public_opthdr/x_image_downscale.m
+++
@@ -1,3 +0,0 @@
-function Ir = x_image_downscale(I, s)
-  Ir = I(1:s:end, 1:s:end, :);
-
