0580893ab57929391b409646783f768edb40cb8e
Jean-Francois Lalonde <jflalonde@gmail.com>
2014-09-30 10:41:35 -0400

    Removed old EXR code.

--- 3rd_party/MatlabEXR/make.m
+++
@@ -1,3 +0,0 @@
-mex exrread.cpp  -lIlmImf -lIex -lImath -lHalf -I/usr/local/include/OpenEXR -L/usr/local/lib
-mex exrwrite.cpp -lIlmImf -lIex -lImath -lHalf -I/usr/local/include/OpenEXR -L/usr/local/lib
-mex exrinfo.cpp  -lIlmImf -lIex -lImath -lHalf -I/usr/local/include/OpenEXR -L/usr/local/lib
